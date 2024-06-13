c8f0793e1ef06cb13b09d7cf1089a7625d5db596
Adam Williams <Adam24Williams@gmail.com>
2024-06-12 20:46:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const content = document.getElementById('content');
     const themeToggle = document.getElementById('theme-toggle');
+    const icon = document.getElementById('icon');
     const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+    function setTheme(theme) {
+        document.documentElement.setAttribute('data-theme', theme);
+        icon.textContent = theme === 'dark' ? '🌜' : '🌞';
+    }
+
     if (userPrefersDark) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        setTheme('dark');
+    } else {
+        setTheme('light');
     }
 
     themeToggle.addEventListener('click', function() {
         const currentTheme = document.documentElement.getAttribute('data-theme');
         const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
+        setTheme(newTheme);
     });
 
     fetch('notes.md')
